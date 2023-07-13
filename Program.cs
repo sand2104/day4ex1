80d957516e7d89a45d6ae0f80932f6c878df272e
sand2104 <prasadsandy2000@gmail.com>
2023-07-13 09:51:43 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter marks in Pre:");
+        int preMarks = Convert.ToInt32(Console.ReadLine());
+
+        if (preMarks > 60)
+        {
+            Console.WriteLine("Enter marks in Final:");
+            int finalMarks = Convert.ToInt32(Console.ReadLine());
+
+            if (finalMarks > 55)
+            {
+                Console.WriteLine("Result: Selected");
+            }
+            else
+            {
+                Console.WriteLine("Result: Fail in Final");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Result: Fail in Pre");
+        }
+
+        Console.ReadLine();
+    }
+}
